115e8cdbd0272c275d4e603a30646aa21e510183
Tyler Huntley <tylershuntley@hotmail.com>
2023-04-17 10:26:39 -0700

    player movement functional

--- team-agreement/space_invaders/space_invaders.py
+++ team-agreement/space_invaders/space_invaders.py
@@ -11,12 +11,12 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Arcade Space Shooter")
 
 # Load images
-RED_SPACE_SHIP = pygame.image.load("assets/pixel_ship_red_small.png")
-BLUE_SPACE_SHIP = pygame.image.load("assets/pixel_ship_blue_small.png")
-GREEN_SPACE_SHIP = pygame.image.load("assets/pixel_ship_green_small.png")
+RED_SHIP = pygame.image.load("assets/pixel_ship_red_small.png")
+BLUE_SHIP = pygame.image.load("assets/pixel_ship_blue_small.png")
+GREEN_SHIP = pygame.image.load("assets/pixel_ship_green_small.png")
 
 # Player ship
-YELLOW_SPACE_SHIP = pygame.image.load("assets/pixel_ship_yellow.png")
+YELLOW_SHIP = pygame.image.load("assets/pixel_ship_yellow.png")
 
 # Lasers
 RED_LASER = pygame.image.load("assets/pixel_laser_red.png")
@@ -28,12 +28,46 @@ YELLOW_LASER = pygame.image.load("assets/pixel_laser_yellow.png")
 BG = pygame.transform.scale(pygame.image.load("assets/background-black.png"), (WIDTH, HEIGHT))
 
 
+class Ship:
+    def __init__(self, x, y, health=100):
+        self.x = x
+        self.y = y
+        self.health = health
+        self.ship_img = None
+        self.laser_img = None
+        self.lasers = []
+        self.cool_down_counter = 0
+
+    def get_width(self):
+        return self.ship_img.get_width()
+
+    def get_height(self):
+        return self.ship_img.get_height()
+
+    def draw(self, window):
+        window.blit(self.ship_img, (self.x, self.y))
+
+
+class Player(Ship):
+    def __init__(self, x, y, health=100):
+        super().__init__(x, y, health)
+        self.ship_img = YELLOW_SHIP
+        self.laser_img = YELLOW_LASER
+        self.mask = pygame.mask.from_surface(self.ship_img)
+        self.max_health = health
+
+
 def main():
     run = True
     FPS = 60
     level = 1
     lives = 5
     main_font = pygame.font.SysFont('arial', 50)
+
+    player_velocity = 5
+
+    player = Player(300, 650)
+
     clock = pygame.time.Clock()
 
 
@@ -44,6 +78,9 @@ def main():
 
         WIN.blit(lives_label, (10, 10))
         WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+        player.draw(WIN)
+
         pygame.display.update()
 
     while run:
@@ -54,8 +91,18 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
 
-    pygame.display.flip()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and player.x - player_velocity > 0:
+            player.x -= player_velocity
+        if keys[pygame.K_RIGHT] and player.x + player.get_width() + player_velocity < WIDTH:
+            player.x += player_velocity
+        if keys[pygame.K_UP] and player.y - player_velocity > 0:
+            player.y -= player_velocity
+        if keys[pygame.K_DOWN] and player.y + player.get_height() + player_velocity < HEIGHT:
+            player.y += player_velocity
 
+
+    pygame.display.flip()
 
 if __name__ == '__main__':
     main()
