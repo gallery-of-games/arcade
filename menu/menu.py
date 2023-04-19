a1830c26ce07a5cdb444cc0a5ee3d6f6825bbfea
Tyler Huntley <tylershuntley@hotmail.com>
2023-04-19 15:20:45 -0700

    integrated snake into main menu and commented out name for the time being

--- menu/menu.py
+++ menu/menu.py
@@ -6,16 +6,19 @@ screen = pygame.display.set_mode((600, 400))
 
 def start_game(game):
     if game == 'Pong':
-        os.system('python3 pong/pong.py')
+        os.system('python pong/pong.py')
     elif game == 'Space Invaders':
-        os.system('python3 space_invaders/space_invaders.py')
+        os.system('python space_invaders/space_invaders.py')
+    elif game == 'Snake':
+        os.system('python snake/snake.py')
 
 menu = pygame_menu.Menu('Welcome', 400, 300,
                        theme=pygame_menu.themes.THEME_BLUE)
 
-name_widget = menu.add.text_input('Name :', default='John Doe')
+# name_widget = menu.add.text_input('Name :', default='John Doe')
 space_invaders_button = menu.add.button('Space Invaders', start_game, 'Space Invaders')
 pong_button = menu.add.button('Pong', start_game, 'Pong')
+pong_button = menu.add.button('Snake', start_game, 'Snake')
 menu.add.button('Quit', pygame_menu.events.EXIT)
 
 
@@ -29,4 +32,4 @@ while True:
     menu.mainloop(screen)
     pygame.display.flip()  # Update the screen
 
-    name_widget.set_value('New Name')  # set the default value of the Name field to "New Name"
+    # name_widget.set_value('New Name')  # set the default value of the Name field to "New Name"
