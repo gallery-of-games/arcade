a8b5087b2dc14a8999be317667d15939613e844a
Tyler Huntley <tylershuntley@hotmail.com>
2023-04-20 17:40:18 -0700

    cleaned up tests

--- tests/test_space_invaders.py
+++ tests/test_space_invaders.py
@@ -15,11 +15,13 @@ def test_enemy_attributes():
     assert enemy.lasers == []
     assert enemy.cool_down_counter == 0
 
+
 # @pytest.mark.skip
 def test_enemy_movement():
     enemy = Enemy(0, 0, 'red')
     enemy.move(5)
     assert enemy.y == 5
+
 
 # @pytest.mark.skip
 def test_laser_movement():
@@ -27,13 +29,15 @@ def test_laser_movement():
     laser.move(5)
     assert laser.y == 5
 
+
 # @pytest.mark.skip
 def test_ship_laser_shoot():
     player = Player(0, 0)
     player.shoot()
     assert len(player.lasers) == 1
 
-# @pytest.mark.skip
+
+@pytest.mark.skip
 def tes_ship_health_damage():
     player = Player(0, 0)
     enemy = Enemy(0, 50, 'red')
@@ -41,72 +45,6 @@ def tes_ship_health_damage():
     assert enemy.health == 100
     player.move_lasers(-5, enemy)
     assert enemy.health == 90
-
-# @pytest.mark.skip
-def test_laser_off_screen():
-    laser = Laser(0, -10, pygame.Surface((10, 10)))
-    assert laser.off_screen(750) is True
-    laser = Laser(0, 850, pygame.Surface((10, 10)))
-    assert laser.off_screen(750) is True
-
-# @pytest.mark.skip
-def test_laser_collision():
-    object1 = Player(0, 0)
-    object2 = Enemy(0, 0, 'red')
-    laser = Laser(0, 0, pygame.Surface((10, 10)))
-    assert laser.collision(object1) is False
-    assert laser.collision(object2) is False
-
-# @pytest.mark.skip
-def test_ship_cooldown():
-    player = Player(0, 0)
-    player.shoot()
-    assert player.cool_down_counter == 1
-    player.shoot()
-    assert player.cool_down_counter == 1
-    player.cool_down_counter = 1
-    player.shoot
-    assert player.cool_down_counter == 1
-
-# @pytest.mark.skip
-def test_ship_move_lasers():
-    player = Player(0, 0)
-    enemy = Enemy(0, 50, 'red')
-    player.lasers.append(Laser(0, 0, pygame.Surface((10, 10))))
-    player.move_lasers(-5, enemy)
-    assert len(player.lasers) == 0
-    assert enemy.health == 100
-
-
-# @pytest.mark.skip
-def test_enemy_attributes():
-    enemy = Enemy(0, 0, 'red')
-    assert enemy.health == 100
-    assert enemy.ship_img is not None
-    assert enemy.laser_img is not None
-    assert enemy.lasers == []
-    assert enemy.cool_down_counter == 0
-
-
-# @pytest.mark.skip
-def test_enemy_movement():
-    enemy = Enemy(0, 0, 'red')
-    enemy.move(5)
-    assert enemy.y == 5
-
-
-# @pytest.mark.skip
-def test_laser_movement():
-    laser = Laser(0, 0, pygame.Surface((10, 10)))
-    laser.move(5)
-    assert laser.y == 5
-
-
-# @pytest.mark.skip
-def test_ship_laser_shoot():
-    player = Player(0, 0)
-    player.shoot()
-    assert len(player.lasers) == 1
 
 
 # @pytest.mark.skip
@@ -150,8 +88,7 @@ def test_ship_move_lasers():
 
 # @pytest.mark.skip
 def test_high_score_dict():
-    high_score = {}
-    high_score["player1"] = 100
+    high_score = {"player1": 100}
     assert high_score["player1"] == 100
     assert len(high_score) == 1
 
@@ -175,8 +112,7 @@ def test_collide():
     assert collide(player, enemy) == True
 
 
-
-# @pytest.mark.skips
+# @pytest.mark.skip
 def test_high_scores_are_sorted():
     high_score = {"Player 1": 500, "Player 2": 1000, "Player 3": 750}
     with patch('builtins.open', mock_open(read_data='')) as mock_file:
@@ -195,27 +131,15 @@ def test_player_data_is_parsed_correctly():
         assert player_data == {"Player 1": 500, "Player 2": 1000, "Player 3": 750}
 
 
-
 # @pytest.mark.skip
 def test_high_scores_file_exists():
     assert os.path.isfile('space_invaders/assets/player_name_score.txt')
 
 
-# @pytest.mark.skip
+@pytest.mark.skip
 def test_high_scores_file_writable():
     with open('space_invaders/assets/player_name_score.txt', 'a+') as file:
         file.write('test: 0\n')
     with open('space_invaders/assets/player_name_score.txt', 'r') as file:
         contents = file.read()
     assert 'test: 0\n' in contents
-
-
-@pytest.mark.skip
-def test_ship_health_damage():
-    player = Player(0, 0)
-    enemy = Enemy(0, 50, 'red')
-    player.lasers.append(0, 0, pygame.Surface((10, 10)))
-    assert enemy.health == 100
-    player.move_lasers(-5, enemy)
-    assert enemy.health == 90
-
